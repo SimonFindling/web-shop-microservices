db8e4e993093431f04c4b895ac4aa4f6acd7ca14
SimonFindling <simon.findling(at)web.de>
2016-11-19 01:23:20 +0100

    added turbine server to aggregate hystrix streams.

--- hystrix-dashboard/src/main/java/de/hska/vislab/hystrix/HystrixDashboardServiceApplication.java
+++ hystrix-dashboard/src/main/java/de/hska/vislab/hystrix/HystrixDashboardServiceApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -12,5 +13,10 @@ public class HystrixDashboardServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HystrixDashboardServiceApplication.class, args);
+	}
+	
+	@RequestMapping("/")
+	public String home() {
+		return "forward:/hystrix";
 	}
 }
---
+++ turbine-server/src/main/java/de/hska/vislab/turbine/TurbineServerApplication.java
@@ -0,0 +1,15 @@
+package de.hska.vislab.turbine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableTurbine
+public class TurbineServerApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(TurbineServerApplication.class, args);
+	}
+}
