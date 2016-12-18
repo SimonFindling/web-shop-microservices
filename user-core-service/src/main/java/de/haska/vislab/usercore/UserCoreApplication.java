f0220029eb9ed43b13236a1df82206569efadc94
Martin Metzger <mema1026@hs-karlsruhe.de>
2016-12-18 12:56:26 +0100

    User Core bla

---
+++ user-core-service/src/main/java/de/haska/vislab/usercore/UserCoreApplication.java
@@ -0,0 +1,15 @@
+package de.haska.vislab.usercore;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+ 
+@SpringBootApplication
+@EnableDiscoveryClient
+public class UserCoreApplication {
+	
+	public static void main(String[] args) {
+        SpringApplication.run(UserCoreApplication.class, args);
+    }
+}
